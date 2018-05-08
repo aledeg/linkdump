0aada5542476b20a4fa3204192ba027a5346ca22
alex-pex <alexandre.paixao@stadline.com>
2018-05-08 12:30:56 +0200

    Apply code formatter on existing code

--- background.js
+++ background.js
@@ -1,49 +1,49 @@
-const linkdumpAddId = "linkdump-add";
+const linkdumpAddId = 'linkdump-add';
 let downloadId = 0;
 
 function addLink(url, title) {
-  browser.storage.local.get("urls")
-  .then(obj => {
+  browser.storage.local.get('urls').then(obj => {
     const urls = obj.urls || [];
-    urls.push({"url": url, "title": title});
+    urls.push({ url, title });
 
-    return browser.storage.local.set({"urls": urls});
+    return browser.storage.local.set({ urls });
   });
 }
 
 // Used by load.content.js
 // eslint-disable-next-line no-unused-vars
 function download() {
-  browser.storage.local.get("urls")
-  .then(obj => {
+  browser.storage.local.get('urls').then(obj => {
     if (!obj.urls) return;
 
-    const content = obj.urls.reduce((a,b) => `${a + b.url  }\n`,'');
-    const blob = new Blob([content], {type: "text/plain"});
+    const content = obj.urls.reduce((a, b) => `${a + b.url}\n`, '');
+    const blob = new Blob([content], { type: 'text/plain' });
 
-    browser.downloads.download({
-      url: URL.createObjectURL(blob),
-      filename: "linkdump.txt",
-      saveAs: true
-    })
-    .then(id => {downloadId = id});
+    browser.downloads
+      .download({
+        url: URL.createObjectURL(blob),
+        filename: 'linkdump.txt',
+        saveAs: true
+      })
+      .then(id => {
+        downloadId = id;
+      });
   });
 }
 
 // Used by load.content.js
 // eslint-disable-next-line no-unused-vars
 function deleteLink(indexes) {
-  browser.storage.local.get("urls")
-  .then(obj => {
+  browser.storage.local.get('urls').then(obj => {
     if (!obj.urls) return;
     const { urls } = obj;
 
     const reversedIndexes = indexes.reverse();
-    reversedIndexes.forEach((item) => {
+    reversedIndexes.forEach(item => {
       urls.splice(item, 1);
     });
 
-    browser.storage.local.set({"urls": urls});
+    browser.storage.local.set({ urls });
   });
 }
 
@@ -51,21 +51,22 @@ function handleChanged(delta) {
   if (delta.id !== downloadId) {
     return;
   }
-  if (delta.state && delta.state.current === "complete") {
-    browser.notifications.create("download-notification", {
-      "type": "basic",
-      "iconUrl": browser.extension.getURL("icons/linkdump-48.png"),
-      "title": "Linkdump",
-      "message": "Download complete"
-    })
-    .then(browser.storage.local.clear());
+  if (delta.state && delta.state.current === 'complete') {
+    browser.notifications
+      .create('download-notification', {
+        type: 'basic',
+        iconUrl: browser.extension.getURL('icons/linkdump-48.png'),
+        title: 'Linkdump',
+        message: 'Download complete'
+      })
+      .then(browser.storage.local.clear());
   }
 }
 
 browser.menus.create({
   id: linkdumpAddId,
-  title: "Add link to dump",
-  contexts: ["link"]
+  title: 'Add link to dump',
+  contexts: ['link']
 });
 
 browser.menus.onClicked.addListener(info => {
--- popup/load.content.js
+++ popup/load.content.js
@@ -1,10 +1,9 @@
-browser.storage.local.get("urls")
-.then(obj => {
+browser.storage.local.get('urls').then(obj => {
   if (obj.urls) {
     obj.urls.forEach((item, index) => {
       const listItem = document.createElement('p');
       const itemSelect = document.createElement('input');
-      itemSelect.id = `link-${  index}`;
+      itemSelect.id = `link-${index}`;
       itemSelect.type = 'checkbox';
       itemSelect.dataset.index = index;
       const itemLink = document.createElement('a');
@@ -14,24 +13,24 @@ browser.storage.local.get("urls")
       listItem.appendChild(itemSelect);
       listItem.appendChild(itemLink);
 
-      document.querySelector("#popup-content").appendChild(listItem);
+      document.querySelector('#popup-content').appendChild(listItem);
     });
   }
 });
 
-document.querySelector("#download").addEventListener("click", () => {
+document.querySelector('#download').addEventListener('click', () => {
   const page = browser.extension.getBackgroundPage();
   page.download();
 });
 
-document.querySelector("#clear").addEventListener("click", () => {
+document.querySelector('#clear').addEventListener('click', () => {
   browser.storage.local.clear();
   window.close();
 });
 
-document.querySelector("#delete").addEventListener("click", () => {
+document.querySelector('#delete').addEventListener('click', () => {
   const indexes = [];
-  document.querySelectorAll('input[type="checkbox"]:checked').forEach((item) => {
+  document.querySelectorAll('input[type="checkbox"]:checked').forEach(item => {
     indexes.push(item.dataset.index);
   });
   const page = browser.extension.getBackgroundPage();
